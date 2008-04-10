bb1a398ee7ad3787d3950b79dd9f539337c8b2a9
rcastaldo <rcastaldo@ca89f3f6-2900-04ea-36f3-08d00da7462f>
2008-04-10 16:33:44 +0000

    importing version 1

---
+++ trunk/TestingApp/Class1.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using TestingApp.Structure;
+
+namespace TestingApp
+{
+	/// <summary>
+	/// Summary description for Class1.
+	/// </summary>
+	class Class1
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+            AccountStatus acs = new AccountStatus();
+            AccountTable a = new AccountTable();
+            Org.Reddragonit.Dbpro.Connections.Firebird.FBConnectionPool pool = new Org.Reddragonit.Dbpro.Connections.Firebird.FBConnectionPool("sysdba", "masterkey", "C:\\Documents and Settings\\rcastaldo\\My Documents\\Firebird\\TESTING.FDB", "localhost", 3050);
+            /*acs.StatusId = 1;
+            acs.StatusName = "Active";
+            acs.Data = System.Text.ASCIIEncoding.ASCII.GetBytes("Hello Joe");
+            a.FirstName = "Roger";
+            a.LastName = "Castaldo";
+            a.Status = acs;*/
+            Org.Reddragonit.Dbpro.Connections.Connection conn = pool.getConnection();
+            conn.CreateTable(acs,true);
+            conn.CreateTable(a,true );
+            /*a=(AccountTable)conn.Save(a);
+            a.FirstName = "George";
+            a = (AccountTable)conn.Save(a);
+            Console.WriteLine(conn.SelectAll(typeof(AccountTable)).Count);
+            List<Org.Reddragonit.Dbpro.Connections.SelectParameter> pars = new List<Org.Reddragonit.Dbpro.Connections.SelectParameter>();
+            pars.Add(new Org.Reddragonit.Dbpro.Connections.SelectParameter("StatusId",a.Status.StatusId ));
+            Console.WriteLine(((AccountStatus)conn.Select(typeof(AccountStatus),pars)[0]).StatusId );*/
+            conn.CloseConnection();
+            pool.ClosePool();
+            Console.ReadLine();
+		}
+	}
+}
