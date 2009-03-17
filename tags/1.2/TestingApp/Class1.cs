4d246b64623dbc4c807d7f4929f1f91038f22a04
Roger <Roger@ca89f3f6-2900-04ea-36f3-08d00da7462f>
2009-03-17 00:22:29 +0000

    integrated in mssql support as well as lazy loading.  Made updating more efficient by tracking changes that are made to a loaded class.

---
+++ tags/1.2/TestingApp/Class1.cs
@@ -0,0 +1,44 @@
+
+using System;
+using System.Data;
+using System.Collections.Generic;
+using TestingApp.Structure;
+using Org.Reddragonit.Dbpro.Connections;
+namespace TestingApp
+{
+	/// <summary>
+	/// Summary description for Class1.
+	/// </summary>
+	class Class1
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			ConnectionPool pool = ConnectionPoolManager.GetConnection("Security");
+			Connection c = pool.getConnection();
+			Group g = new Group();
+			g.InheritParentRights=false;
+			g.Name="Admin";
+			g=Group.Save(g);
+			User u= new User();
+			u.FirstName="Roger";
+			u.LastName="Castaldo";
+			u.UserGroup=g;
+			u.UserName="rcastaldo";
+			u.Password="copperbed1";
+			u = User.Save(u);
+			
+			u = User.LoginUser("rcastaldo","copperbed1");
+			if (u==null)
+				Console.WriteLine("Unable to login user.");
+			else
+				Console.WriteLine("User logged in.");
+			
+            pool.ClosePool();
+            Console.ReadLine();
+		}
+	}
+}
