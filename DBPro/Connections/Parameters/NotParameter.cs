688799c4b2c0b99543ea2393e78054fa467fff08
Roger_Castaldo <Roger_Castaldo@ca89f3f6-2900-04ea-36f3-08d00da7462f>
2009-10-13 17:20:03 +0000

    Added Class SQL based selection.  Need to finish code for handling table fields and not field fields, also need to add in code for handling of parameters.

---
+++ DBPro/Connections/Parameters/NotParameter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Org.Reddragonit.Dbpro.Connections.Parameters
+{
+    public class NotParameter : SelectParameter
+    {
+        private SelectParameter _negatedParameter;
+
+        public NotParameter(SelectParameter negatedParameter)
+        {
+            _negatedParameter = negatedParameter;
+        }
+
+        internal override string ConstructString(Org.Reddragonit.Dbpro.Structure.Mapping.TableMap map, Connection conn, QueryBuilder builder, ref List<System.Data.IDbDataParameter> queryParameters, ref int parCount)
+        {
+            return "NOT ( " + _negatedParameter.ConstructString(map, conn, builder, ref queryParameters, ref parCount) + " ) ";
+        }
+    }
+}
