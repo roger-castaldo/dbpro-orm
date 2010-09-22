e889401438ab447ca469963cb3559a3e6caf75b9
roger_castaldo <roger_castaldo@ca89f3f6-2900-04ea-36f3-08d00da7462f>
2010-09-22 13:59:19 +0000

    added in ability to register class based triggers to occur when certain actions are performed.

---
+++ DBPro/Connections/ITrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Org.Reddragonit.Dbpro.Structure;
+
+namespace Org.Reddragonit.Dbpro.Connections
+{
+    public interface ITrigger
+    {
+        void PostUpdate(Table table, List<string> changedFields);
+        void PreUpdate(Table table, List<string> changedFields);
+        void PreDelete(Table table);
+        void PostDelete(Table table);
+        void PreDeleteAll();
+        void PostDeleteAll();
+        void PreInsert(Table table);
+        void PostInsert(Table table);
+    }
+}
