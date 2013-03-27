ab8caf75b5e97b1ef09044578c17b3d7e4d89378
roger.castaldo@gmail.com <roger.castaldo@gmail.com@ca89f3f6-2900-04ea-36f3-08d00da7462f>
2013-03-27 18:54:33 +0000

    added in select conditions for starts with and ends with

---
+++ DBPro/Connections/Parameters/EndsWithParameter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Org.Reddragonit.Dbpro.Connections.Parameters
+{
+    public class EndsWithParameter : CompareParameter
+    {
+        public EndsWithParameter(string fieldName, string fieldValue)
+            : base(fieldName, "%"+fieldValue.ToString().ToUpper())
+        {
+        }
+
+        protected override string ComparatorString
+        {
+            get { return "LIKE"; }
+        }
+
+        protected override bool CaseInsensitive
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
