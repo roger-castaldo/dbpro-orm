a30cfae52b9bbf3d5e70d7cba67b873c08416681
roger.castaldo@gmail.com <roger.castaldo@gmail.com@ca89f3f6-2900-04ea-36f3-08d00da7462f>
2013-10-07 15:42:27 +0000

    Add in attribute class for registering triggers on initialization.  Requires an empty constructor in the Trigger Class

---
+++ trunk/DBPro/Connections/TriggerRegisterAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Org.Reddragonit.Dbpro.Connections
+{
+    [AttributeUsage(AttributeTargets.Class,AllowMultiple=true)]
+    public class TriggerRegisterAttribute : Attribute
+    {
+        private Type _table;
+        public Type Table
+        {
+            get { return _table; }
+        }
+
+        public TriggerRegisterAttribute(Type table)
+        {
+            _table = table;
+        }
+    }
+}
